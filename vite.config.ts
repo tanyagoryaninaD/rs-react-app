d56badcec885ff222a0b00569b21bc98cc4816d7
Tatsiana Haranina <tanyagoryanina.dev@gmail.com>
2025-07-18 14:04:43 +0300

    chore: add config Vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/__test__/setupTests.ts',
+    include: ['./src/__test__', 'src/**/*.test.{js,ts,tsx}'],
+    exclude: ['node_modules', 'dist'],
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'text-summary'],
+      reportsDirectory: './coverage',
+      all: true,
+      include: ['src/**/*.{js,ts,tsx}'],
+      exclude: [
+        './src/__test__',
+        'src/**/*.test.{js,ts,tsx}',
+        'node_modules',
+        'dist',
+      ],
+    },
+    watch: false,
+    reporters: ['default'],
+    maxConcurrency: 5,
+    testTimeout: 10000,
+    mockReset: true,
+  },
 });
