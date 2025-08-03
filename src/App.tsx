6b162928042f7ed0c363dd57a54772a08906aaa5
tanyagoryaninad <tanyagoryanina.dev@gmail.com>
2025-08-03 19:49:55 +0300

    feat: add context to control the application theme (light or dark)

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,42 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import { About } from './components/About/About';
 import { NotFound } from './components/NotFound/NotFound';
 import { Main } from './components/Main';
+import { useEffect, useState } from 'react';
+import { ThemeContext, type Theme } from './types/contexts';
+import { useLocalStorage } from './utils/localStorage';
 
 export function App(): ReactNode {
+  const [stateStorageTheme, setStateStorageTheme] = useLocalStorage<Theme>(
+    'tg-theme',
+    'light'
+  );
+  const [theme, setTheme] = useState<Theme>(stateStorageTheme);
+
+  useEffect(() => {
+    setStateStorageTheme(theme);
+
+    document.body.classList.toggle('dark', theme === 'dark');
+    document.body.classList.toggle('light', theme === 'light');
+  }, [setStateStorageTheme, theme]);
+
+  const toggleTheme = () => {
+    setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+  };
+
   return (
     <>
       <ErrorBoundary>
-        <Router>
-          <Routes>
-            <Route path="/" element={<Main />}>
-              <Route index element={<SearchPanel />} />
-              <Route path="/about" element={<About />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />\
-          </Routes>
-        </Router>
+        <ThemeContext value={{ theme, toggleTheme }}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Main />}>
+                <Route index element={<SearchPanel />} />
+                <Route path="/about" element={<About />} />
+              </Route>
+              <Route path="*" element={<NotFound />} />\
+            </Routes>
+          </Router>
+        </ThemeContext>
       </ErrorBoundary>
     </>
   );
