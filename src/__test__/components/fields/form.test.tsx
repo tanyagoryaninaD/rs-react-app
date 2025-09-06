108fb1f0b7ade129198d630a2f23fa5a8aed54e0
tanyagoryaninad <tanyagoryanina.dev@gmail.com>
2025-09-06 03:38:23 +0300

    test: improve test modal, remove comments

--- src/__test__/components/fields/form.test.tsx
+++ src/__test__/components/fields/form.test.tsx
@@ -43,30 +43,4 @@ describe('Forms components', () => {
     expect(screen.getByTestId('email-error')).toBeInTheDocument();
     expect(screen.getByTestId('password-error')).toBeInTheDocument();
   });
-
-  // it('submit', async () => {
-  //   const button = screen.getByRole('button', { name: 'Submit' });
-
-  //   await userEvent.type(screen.getByTestId('name'), 'Test');
-  //   await userEvent.type(screen.getByTestId('age'), '20');
-  //   await userEvent.type(screen.getByTestId('email'), 'test@gmail.com');
-  //   await userEvent.type(screen.getByTestId('password'), '!Qw2');
-
-  //   await userEvent.type(screen.getByTestId('repeatPassword'), '!Qw2');
-  //   await userEvent.type(screen.getByTestId('country'), 'Belarus');
-  //   await userEvent.click(screen.getByTestId('accept'));
-  //   await userEvent.click(screen.getByTestId('gender-man'));
-
-  //   const fileInput = screen.getByTestId('file') as HTMLInputElement;
-  //   const file = new File(['content'], 'test.png', {
-  //     type: 'image/png',
-  //     lastModified: 1,
-  //   }) as unknown as File;
-  //   await userEvent.upload(fileInput, file);
-
-  //   console.log('After change:', fileInput.files?.[0]);
-
-  //   await userEvent.click(button);
-  //   screen.debug();
-  // });
 });
--- src/__test__/components/modal.test.tsx
+++ src/__test__/components/modal.test.tsx
@@ -1,39 +1,48 @@
 import { render, screen } from '@testing-library/react';
 import { describe, it, beforeEach, expect } from 'vitest';
 import userEvent from '@testing-library/user-event';
-import Modal from '../../app/components/modal/modal';
-import { onClose } from '../mocks';
-import Form from '../../app/components/forms/form';
+import App from '../../app/App';
 
 describe('App component', () => {
   beforeEach(() => {
-    render(
-      <Modal isOpen={true} onClose={onClose}>
-        <Form onClose={onClose} />
-      </Modal>
-    );
+    render(<App />);
   });
 
   it('clicks on Close button should closed modal', async () => {
-    const closeButton = screen.getByTestId('close-modal');
+    const button = screen.getByRole('button', { name: 'Form' });
+    await userEvent.click(button);
+    const modal = screen.queryByTestId('modal');
 
+    expect(modal).toBeInTheDocument();
+
+    const closeButton = screen.getByTestId('close-modal');
     await userEvent.click(closeButton);
 
-    expect(onClose).toBeCalled();
+    expect(modal).not.toBeInTheDocument();
   });
 
   it('clicks on overlay should closed  modal', async () => {
+    const button = screen.getByRole('button', { name: 'Form' });
+    await userEvent.click(button);
+    const modal = screen.queryByTestId('modal');
+
+    expect(modal).toBeInTheDocument();
     await userEvent.click(screen.getByTestId('overlay'));
 
-    expect(onClose).toBeCalled();
+    expect(modal).not.toBeInTheDocument();
   });
 
   it('clicks on Escape should closed  modal', async () => {
+    const button = screen.getByRole('button', { name: 'Form' });
+    await userEvent.click(button);
     const modal = screen.getByTestId('modal');
+
+    expect(modal).toBeInTheDocument();
+
     modal.focus();
 
     await userEvent.keyboard('{Escape}');
 
-    expect(onClose).toBeCalled();
+    expect(modal).not.toBeInTheDocument();
   });
 });
