58a5bdf5027bbb388919d795123da993d60d1bba
tanyagoryaninad <tanyagoryanina.dev@gmail.com>
2025-09-06 04:23:12 +0300

    refactor: remove unused component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,10 @@
-import { Suspense } from 'react';
 import { SearchPanel } from '../../components/SearchPanel/SearchPanel';
 import MainLayout from './(main)/MainLayout';
 
 export default async function Page() {
   return (
     <MainLayout>
-      <Suspense>
-        <SearchPanel />
-      </Suspense>
+      <SearchPanel />
     </MainLayout>
   );
 }
